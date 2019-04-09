dbbdaeb5ed674a944d639265c4a2c3926e42251f
unknown <eboehm2@gmail.com>
2019-04-08 19:34:39 -0700

    finished get endpoints for car model

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getALLCars(model: any): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    getCarById(model: any): (req: Request, res: Response, next: NextFunction) => Promise<void>;
     model: any;
 }
